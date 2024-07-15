50e1d0cce6e88f16fc070bb42f7fa0c0f33a8654
Ayush2832 <ayush28.connect@gmail.com>
2024-07-15 11:25:49 +0530

    file is modified

--- hello.go
+++ hello.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello world")
+	a := 10
+	b := 20
+
+	if a > b{
+		fmt.Println ( " A is greater than B which is 10" )
+	}else{
+		fmt.Println( " B is greater than A which is 20" )
+	}
 }
